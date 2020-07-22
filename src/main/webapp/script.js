d4935d219d490fdb66837e4e65f0169abfd1a912
otobin <oliviatobin31@gmail.com>
2020-07-21 22:04:28 -0700

    Added code for state visualization

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -10,11 +10,43 @@ function passQuery() {
         .then((data) => { 
           // data is a 2D array, where the first row is a header row and all
           // subsequent rows are one piece of data (e.g. for a state or county)
-          callOliviaFunction(data);
+          displayVisualization(data);
         });
       }
       else {
         console.log("There was an error");
       }
     });
+}
+
+// displayVisualization takes in a data array, which is a 2D array that 
+function displayVisualization(dataArray) {
+  google.charts.load('current', {
+    'packages':['geochart'],
+    // Note: you will need to get a mapsApiKey for your project.
+    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+  });
+  google.charts.setOnLoadCallback(drawRegionsMap(dataArray));
+}
+
+function drawRegionsMap(dataArray) {
+  const shortDataArray = [];
+  dataArray.forEach((state) => {
+    shortDataArray.push([state[0], state[1]/1000]);
+  });
+  shortDataArray[0][0] = 'State';
+  shortDataArray[0][1] = 'Population';
+  console.log(shortDataArray);
+  var data = google.visualization.arrayToDataTable(shortDataArray);
+
+  var options = {
+    'region': 'US',
+    'resolution': 'provinces',
+    'colorAxis': {colors: ['white', 'blue']}
+  };
+
+  var chart = new google.visualization.GeoChart(document.getElementById('visualization'));
+
+  chart.draw(data, options);
 }
