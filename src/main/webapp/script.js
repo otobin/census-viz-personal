b3c025e399520fc3d557ccaa0b56854ac7eee10f
kyracq <kyraa@google.com>
2020-07-22 12:53:27 -0400

    Add caching using service worker

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,3 +1,16 @@
+function regiesterServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(response) {
+      // Service worker registration done
+      console.log('Registration Successful', response);
+    }, function(error) {
+      // Service worker registration failed
+      console.log('Registration Failed', error);
+    });
+  }
+}
+
 function passQuery() {
   const query = new FormData(document.getElementById('query-form'));
   const personType = query.get('person-type');
@@ -10,43 +23,11 @@ function passQuery() {
         .then((data) => { 
           // data is a 2D array, where the first row is a header row and all
           // subsequent rows are one piece of data (e.g. for a state or county)
-          displayVisualization(data);
+          console.log(data);
         });
       }
       else {
         console.log("There was an error");
       }
     });
-}
-
-// displayVisualization takes in a data array, which is a 2D array that 
-function displayVisualization(dataArray) {
-  google.charts.load('current', {
-    'packages':['geochart'],
-    // Note: you will need to get a mapsApiKey for your project.
-    // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-    'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
-  });
-  google.charts.setOnLoadCallback(drawRegionsMap(dataArray));
-}
-
-function drawRegionsMap(dataArray) {
-  const shortDataArray = [];
-  dataArray.forEach((state) => {
-    shortDataArray.push([state[0], state[1]/1000]);
-  });
-  shortDataArray[0][0] = 'State';
-  shortDataArray[0][1] = 'Population';
-  console.log(shortDataArray);
-  var data = google.visualization.arrayToDataTable(shortDataArray);
-
-  var options = {
-    'region': 'US',
-    'resolution': 'provinces',
-    'colorAxis': {colors: ['white', 'blue']}
-  };
-
-  var chart = new google.visualization.GeoChart(document.getElementById('visualization'));
-
-  chart.draw(data, options);
 }
---
+++ src/main/webapp/service-worker.js
@@ -0,0 +1,58 @@
+const CACHE_VERSION = 1;
+const CURRENT_CACHES = {
+  censusData: 'census-data-cache-v' + CACHE_VERSION,
+};
+
+
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHES.
+  // Useful if multiple versioned caches
+  const expectedCacheNamesSet = new Set(Object.values(CURRENT_CACHES));
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if(!expectedCacheNamesSet.has(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    })
+  )
+});
+
+// Check cache and cache data if necessary on every fetch()
+self.addEventListener('fetch', function(event) {
+  console.log('Handling fetch event for', event.request.url);
+  event.respondWith(
+    caches.open(CURRENT_CACHES.censusData).then(function(cache) {
+      return cache.match(event.request).then(function(response) {
+        if (response) {
+          // response found in cache
+          console.log('Response found in cache');
+          return response;
+        }
+        // response not found in cache, we need to fetch
+        // make a copy of the request because fetch "consumes" a request
+        // and we might need to cache it
+        return fetch(event.request.clone()).then(function(response) {
+          if(response.status < 300 &&
+            response.headers.has('content-type') &&
+            response.headers.get('content-type').match(/application\/json/)) {
+              // only cache if non-error response
+              // make a copy of response because put() "consumes" requests
+              // and we will return original response object
+              console.log('Caching response');
+              cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      }).catch(function(error) {
+        // Handle exceptions from match() or fetch()
+        // Note: HTTP error response does not trigger exception
+        console.error('Error in fetch handler:', error);
+        throw error;
+      })
+    })
+  );
+});
