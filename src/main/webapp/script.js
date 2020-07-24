1ce4c34ed534ff1ad01d9fd1409b00707a99238c
kyracq <kyraa@google.com>
2020-07-24 18:38:41 -0400

    Update script.js

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -19,69 +19,75 @@ google.charts.load('current', {
 });
 
 function passQuery() {
-  document.getElementById('result').style.display = 'block';
+  const information = document.getElementById('more-info');
+  information.innerText = 'Please wait. Loading...';
   const query = new FormData(document.getElementById('query-form'));
   const personType = query.get('person-type');
+  const action = query.get('action');
   const location = query.get('location');
 
-  fetch('/query?person-type=' + personType + '&location=' + location)
-      .then((response) => {
-        if (response.ok) {
-          response.json().then((jsonResponse) => JSON.parse(jsonResponse))
-              .then((data) => {
-                // data is a 2D array, where the first row is a header row and all
-                // subsequent rows are one piece of data (e.g. for a state or county)
-                displayVisualization(data);
-              });
-        } else {
-          console.log('There was an error');
-        }
-      });
+  const fetchUrl = '/query?person-type=' + personType +
+      '&action=' + action +
+      '&location=' + location;
+  
+  fetch(fetchUrl)
+    .then((response) => {
+      if (response.ok) {
+        response.json().then((jsonResponse) => JSON.parse(jsonResponse))
+        .then((data) => {
+          // data is a 2D array, where the first row is a header row and all
+          // subsequent rows are one piece of data (e.g. for a state or county)
+          information.innerText = '';
+          displayVisualization(data);
+        });
+      } else {
+        console.log(
+          `An error occurred: ${response.status}: ${response.statusText}`);
+      }
+    });
 }
 
-// displayVisualization takes in a data array representing the 2D array returned by the
-// census API. The function creates the necessary visualization with the census data.
+// displayVisualization takes in a data array representing the 2D array
+// returned  by the census API. The first row in the census Data array
+// is the header, which describes the type of data.
+// Eg: Header for the query that finds for populations of all counties is
+// ["NAME","S0201_001E","state","county"]
 function displayVisualization(censusDataArray) {
   if (censusDataArray[0][2] == 'state' && censusDataArray[0][3] != 'county') {
     google.charts.setOnLoadCallback(drawRegionsMap(censusDataArray));
   } else {
     // We currently do not have counties implemented
+    const errorMessage = 'We do not support this visualization yet';
     document.getElementById('map').innerHTML = '';
-    document.getElementById('more-info').innerHTML = 'We do not have this information yet.';
+    document.getElementById('more-info').innerHTML = errorMessage;
   }
 }
 
-// Takes in a 2D array from the census API and draws the appropriate visualization
+// Takes in a 2D array from the census API and displays the visualization
 function drawRegionsMap(censusDataArray) {
   const shortDataArray = createDataArray(censusDataArray);
   const data = google.visualization.arrayToDataTable(shortDataArray);
   const options = {
-    colorAxis: {colors: ['white', 'blue']},
-    height: 550,
-    region: 'US',
-    resolution: 'provinces',
+    'region': 'US',
+    'resolution': 'provinces',
+    'colorAxis': {colors: ['white', 'blue']},
   };
-  document.getElementById('result').style.display = 'block';
-  const chart = new google.visualization.GeoChart(document.getElementById('map'));
+  const mapElement = document.getElementById('map');
+  const chart = new google.visualization.GeoChart(mapElement);
   chart.draw(data, options);
-  document.getElementById('more-info').innerHTML =
-      'These populations are divided by 1000';
+  const mapNote = 'Populations are in thousands';
+  document.getElementById('more-info').innerHTML = mapNote;
 }
 
-// createDataArray takes in the data array returned by the census API and reformats it
-// into a data table that can be processed by the visualization API.
+// createDataArray takes in the data array returned by the census API
+// and reformats it into a data table for the visualization API.
 function createDataArray(censusDataArray) {
   const vizDataArray = [];
   censusDataArray.forEach((state) => {
     vizDataArray.push([state[0], state[1]/1000]);
   });
+  // Changes the header of the vizDataArray to match the Visualization API
   vizDataArray[0][0] = 'State';
   vizDataArray[0][1] = 'Population';
   return vizDataArray;
 }
-
-function resizeVisualization() {
-  passQuery();
-}
-
-window.addEventListener('resize', resizeVisualization);
