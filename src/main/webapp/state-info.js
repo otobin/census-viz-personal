0c38150534e8ce1a7449a351011e054c195bf711
kyracq <kyraa@google.com>
2020-07-30 19:20:05 -0400

    linting fixes

--- src/main/webapp/state-info.js
+++ src/main/webapp/state-info.js
@@ -1,210 +1,210 @@
 const stateInfo = {
-    "01": {
-        "name": "Alabama",
-        "ISO": "US-AL"
+    '01': {
+        'name': 'Alabama',
+        'ISO': 'US-AL',
     },
-    "02": {
-        "name": "Alaska",
-        "ISO": "US-AK"
+    '02': {
+        'name': 'Alaska',
+        'ISO': 'US-AK',
     },
-    "04": {
-        "name": "Arizona",
-        "ISO": "US-AZ"
+    '04': {
+        'name': 'Arizona',
+        'ISO': 'US-AZ',
     },
-    "05": {
-        "name": "Arkansas",
-        "ISO": "US-AR"
+    '05': {
+        'name': 'Arkansas',
+        'ISO': 'US-AR',
     },
-    "06": {
-        "name": "California",
-        "ISO": "US-CA"
+    '06': {
+        'name': 'California',
+        'ISO': 'US-CA',
     },
-    "08": {
-        "name": "Colorado",
-        "ISO": "US-CO"
+    '08': {
+        'name': 'Colorado',
+        'ISO': 'US-CO',
     },
-    "09": {
-        "name": "Connecticut",
-        "ISO": "US-CT"
+    '09': {
+        'name': 'Connecticut',
+        'ISO': 'US-CT',
     },
-    "10": {
-        "name": "Delaware",
-        "ISO": "US-DE"
+    '10': {
+        'name': 'Delaware',
+        'ISO': 'US-DE',
     },
-    "11": {
-        "name": "District of Columbia",
-        "ISO": "US-DC"
+    '11': {
+        'name': 'District of Columbia',
+        'ISO': 'US-DC',
     },
-    "12": {
-        "name": "Florida",
-        "ISO": "US-FL"
+    '12': {
+        'name': 'Florida',
+        'ISO': 'US-FL',
     },
-    "13": {
-        "name": "Georgia",
-        "ISO": "US-GA"
+    '13': {
+        'name': 'Georgia',
+        'ISO': 'US-GA',
     },
-    "15": {
-        "name": "Hawaii",
-        "ISO": "US-HI"
+    '15': {
+        'name': 'Hawaii',
+        'ISO': 'US-HI',
     },
-    "16": {
-        "name": "Idaho",
-        "ISO": "US-ID"
+    '16': {
+        'name': 'Idaho',
+        'ISO': 'US-ID',
     },
-    "17": {
-        "name": "Illinois",
-        "ISO": "US-IL"
+    '17': {
+        'name': 'Illinois',
+        'ISO': 'US-IL',
     },
-    "18": {
-        "name": "Indiana",
-        "ISO": "US-IN"
+    '18': {
+        'name': 'Indiana',
+        'ISO': 'US-IN',
     },
-    "19": {
-        "name": "Iowa",
-        "ISO": "US-IA"
+    '19': {
+        'name': 'Iowa',
+        'ISO': 'US-IA',
     },
-    "20": {
-        "name": "Kansas",
-        "ISO": "US-KS"
+    '20': {
+        'name': 'Kansas',
+        'ISO': 'US-KS',
     },
-    "21": {
-        "name": "Kentucky",
-        "ISO": "US-KY"
+    '21': {
+        'name': 'Kentucky',
+        'ISO': 'US-KY',
     },
-    "22": {
-        "name": "Louisiana",
-        "ISO": "US-LA"
+    '22': {
+        'name': 'Louisiana',
+        'ISO': 'US-LA',
     },
-    "23": {
-        "name": "Maine",
-        "ISO": "US-ME"
+    '23': {
+        'name': 'Maine',
+        'ISO': 'US-ME',
     },
-    "24": {
-        "name": "Maryland",
-        "ISO": "US-MD"
+    '24': {
+        'name': 'Maryland',
+        'ISO': 'US-MD',
     },
-    "25": {
-        "name": "Massachusetts",
-        "ISO": "US-MA"
+    '25': {
+        'name': 'Massachusetts',
+        'ISO': 'US-MA',
     },
-    "26": {
-        "name": "Michigan",
-        "ISO": "US-MI"
+    '26': {
+        'name': 'Michigan',
+        'ISO': 'US-MI',
     },
-    "27": {
-        "name": "Minnesota",
-        "ISO": "US-MN"
+    '27': {
+        'name': 'Minnesota',
+        'ISO': 'US-MN',
     },
-    "28": {
-        "name": "Mississippi",
-        "ISO": "US-MS"
+    '28': {
+        'name': 'Mississippi',
+        'ISO': 'US-MS',
     },
-    "29": {
-        "name": "Missouri",
-        "ISO": "US-MO"
+    '29': {
+        'name': 'Missouri',
+        'ISO': 'US-MO',
     },
-    "30": {
-        "name": "Montana",
-        "ISO": "US-MT"
+    '30': {
+        'name': 'Montana',
+        'ISO': 'US-MT',
     },
-    "31": {
-        "name": "Nebraska",
-        "ISO": "US-NE"
+    '31': {
+        'name': 'Nebraska',
+        'ISO': 'US-NE',
     },
-    "32": {
-        "name": "Nevada",
-        "ISO": "US-NV"
+    '32': {
+        'name': 'Nevada',
+        'ISO': 'US-NV',
     },
-    "33": {
-        "name": "New Hampshire",
-        "ISO": "US-NH"
+    '33': {
+        'name': 'New Hampshire',
+        'ISO': 'US-NH',
     },
-    "34": {
-        "name": "New Jersey",
-        "ISO": "US-NJ"
+    '34': {
+        'name': 'New Jersey',
+        'ISO': 'US-NJ',
     },
-    "35": {
-        "name": "New Mexico",
-        "ISO": "US-NM"
+    '35': {
+        'name': 'New Mexico',
+        'ISO': 'US-NM',
     },
-    "36": {
-        "name": "New York",
-        "ISO": "US-NY"
+    '36': {
+        'name': 'New York',
+        'ISO': 'US-NY',
     },
-    "37": {
-        "name": "North Carolina",
-        "ISO": "US-NC"
+    '37': {
+        'name': 'North Carolina',
+        'ISO': 'US-NC',
     },
-    "38": {
-        "name": "North Dakota",
-        "ISO": "US-ND"
+    '38': {
+        'name': 'North Dakota',
+        'ISO': 'US-ND',
     },
-    "39": {
-        "name": "Ohio",
-        "ISO": "US-OH"
+    '39': {
+        'name': 'Ohio',
+        'ISO': 'US-OH',
     },
-    "40": {
-        "name": "Oklahoma",
-        "ISO": "US-OK"
+    '40': {
+        'name': 'Oklahoma',
+        'ISO': 'US-OK',
     },
-    "41": {
-        "name": "Oregon",
-        "ISO": "US-OR"
+    '41': {
+        'name': 'Oregon',
+        'ISO': 'US-OR',
     },
-    "42": {
-        "name": "Pennsylvania",
-        "ISO": "US-PA"
+    '42': {
+        'name': 'Pennsylvania',
+        'ISO': 'US-PA',
     },
-    "44": {
-        "name": "Rhode Island",
-        "ISO": "US-RI"
+    '44': {
+        'name': 'Rhode Island',
+        'ISO': 'US-RI',
     },
-    "45": {
-        "name": "South Carolina",
-        "ISO": "US-SC"
+    '45': {
+        'name': 'South Carolina',
+        'ISO': 'US-SC',
     },
-    "46": {
-        "name": "South Dakota",
-        "ISO": "US-SD"
+    '46': {
+        'name': 'South Dakota',
+        'ISO': 'US-SD',
     },
-    "47": {
-        "name": "Tennessee",
-        "ISO": "US-TN"
+    '47': {
+        'name': 'Tennessee',
+        'ISO': 'US-TN',
     },
-    "48": {
-        "name": "Texas",
-        "ISO": "US-TX"
+    '48': {
+        'name': 'Texas',
+        'ISO': 'US-TX',
     },
-    "49": {
-        "name": "Utah",
-        "ISO": "US-UT"
+    '49': {
+        'name': 'Utah',
+        'ISO': 'US-UT',
     },
-    "50": {
-        "name": "Vermont",
-        "ISO": "US-VT"
+    '50': {
+        'name': 'Vermont',
+        'ISO': 'US-VT',
     },
-    "51": {
-        "name": "Virginia",
-        "ISO": "US-VA"
+    '51': {
+        'name': 'Virginia',
+        'ISO': 'US-VA',
     },
-    "53": {
-        "name": "Washington",
-        "ISO": "US-WA"
+    '53': {
+        'name': 'Washington',
+        'ISO': 'US-WA',
     },
-    "54": {
-        "name": "West Virginia",
-        "ISO": "US-WV"
+    '54': {
+        'name': 'West Virginia',
+        'ISO': 'US-WV',
     },
-    "55": {
-        "name": "Wisconsin",
-        "ISO": "US-WI"
+    '55': {
+        'name': 'Wisconsin',
+        'ISO': 'US-WI',
     },
-    "56": {
-        "name": "Wyoming",
-        "ISO": "US-WY"
+    '56': {
+        'name': 'Wyoming',
+        'ISO': 'US-WY',
     },
-    "72": {
-        "name": "Puerto Rico",
-        "ISO": "US-PR"
-    }
-}
+    '72': {
+        'name': 'Puerto Rico',
+        'ISO': 'US-PR',
+    },
+};
