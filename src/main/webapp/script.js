3b074a95118ed454ee3c2b8acb73b7f3dc125dbd
kyracq <kyraa@google.com>
2020-07-30 18:48:50 -0400

    Switch state map to amcharts, no zoom on scroll

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -13,21 +13,6 @@ function registerServiceWorker() {
         });
   }
 }
-google.charts.load('current', {
-  'packages': ['geochart'],
-  'mapsApiKey': 'AIzaSyB5cba6r-suEYL-0E_nRQfXDtT4XW0WxbQ',
-});
-
-const states = {
-  'California': {
-    number: '06',
-    geoData: am4geodata_region_usa_caLow,
-  },
-  'New Jersey': {
-    number: '34',
-    geoData: am4geodata_region_usa_njLow,
-  },
-};
 
 function passQuery() {
   document.getElementById('map-title').innerText = '';
@@ -55,7 +40,7 @@ function passQuery() {
   }
   const description = {action: action, age: personType};
   const isCountyQuery = location !== 'state';
-  const region = isCountyQuery ? locationName + ' county' : 'U.S. state';
+  const region = isCountyQuery ? locationInput + ' county' : 'U.S. state';
   const title = 'Population who ' + action +
       ' in each ' + region + ' (' + personType.replace('-', ' ') + ')';
   document.getElementById('map-title').innerText = title;
@@ -64,26 +49,16 @@ function passQuery() {
     '&action=' + action +
     '&location=' + location;
 
-  if (isCountyQuery) {
-    fetchUrl += '&state-number=' + states[locationName].number;
-  } else {
-    fetchUrl += '&state-number=';
-  }
-
   fetch(fetchUrl)
     .then((response) => {
       if (response.ok) {
         response.json().then((jsonResponse) => JSON.parse(jsonResponse))
-        .then((data) => {
+        .then((censusDataArray) => {
           // data is a 2D array, where the first row is a header row and all
           // subsequent rows are one piece of data (e.g. for a state or county)
-          information.innerText = '';
-
-          if (isCountyQuery) {
-            displayAmChartsMap(data, locationName);
-          } else {
-            displayVisualization(data, description, title);
-          }
+          const data = createDataArray(censusDataArray, isCountyQuery);
+          displayAmChartsMap(data, description, location, isCountyQuery);
+          document.getElementById('more-info').innerText = '';
         });
       } else {
         displayError(response.status, response.statusText);
@@ -91,13 +66,29 @@ function passQuery() {
     });
 }
 
-function displayAmChartsMap(data, stateName) {
+function displayAmChartsMap(data, description, location, isCountyQuery) {
   am4core.useTheme(am4themes_animated);
   const chart = am4core.create('map', am4maps.MapChart);
   chart.height = 550;
-
+  chart.zoomControl = new am4maps.ZoomControl();
+  // only allow zooming with buttons
+  chart.mouseWheelBehavior = 'none';
+  
   // Create map instance
-  chart.geodata = states[stateName].geoData;
+  if (isCountyQuery) {
+    chart.geodata = location === '06' ? am4geodata_region_usa_caLow : am4geodata_region_usa_njLow;
+  } else {
+    chart.geodata = am4geodata_usaLow;
+  }
+
+  // Add button to zoom out
+  const home = chart.chartContainer.createChild(am4core.Button);
+  home.label.text = "Home";
+  home.align = "right";
+  home.events.on("hit", function(ev) {
+    chart.goHome();
+  });
+
   chart.projection = new am4maps.projections.AlbersUsa();
   const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
 
@@ -110,11 +101,7 @@ function displayAmChartsMap(data, stateName) {
     logarithmic: true,
   });
   polygonSeries.useGeodata = true;
-
-  const countyData = data.map((value) => {
-      return {id: value[2] + value[3], value: value[1]};
-  });
-  polygonSeries.data = countyData;
+  polygonSeries.data = data;
 
   // Set up heat legend
   const heatLegend = chart.createChild(am4maps.HeatLegend);
@@ -133,7 +120,13 @@ function displayAmChartsMap(data, stateName) {
 
   // Configure series tooltip
   const polygonTemplate = polygonSeries.mapPolygons.template;
-  polygonTemplate.tooltipText = '{name}: {value}';
+  let descriptionString = 'Population of people ' +
+    description.age + ' who ' + description.action;
+  // make it moved to
+  if (description.action === 'moved') {
+    descriptionString += ' to';
+  }
+  polygonTemplate.tooltipText = '{name}\n' + descriptionString + ': {value}';
   polygonTemplate.nonScalingStroke = true;
   polygonTemplate.strokeWidth = 0.5;
 
@@ -146,8 +139,8 @@ function displayAmChartsMap(data, stateName) {
   polygonSeries.mapPolygons.template.events.on('over', function(event) {
     handleHover(event.target);
   });
-  polygonSeries.mapPolygons.template.events.on('hit', function(event) {
-    handleHover(event.target);
+  polygonTemplate.events.on('hit', function (event) {
+    chart.zoomToMapObject(event.target);
   });
   function handleHover(column) {
     if (!isNaN(column.dataItem.value)) {
@@ -159,6 +152,8 @@ function displayAmChartsMap(data, stateName) {
   polygonSeries.mapPolygons.template.events.on('out', function(event) {
     heatLegend.valueAxis.hideTooltip();
   });
+}
+
 function validateInput(dataListId, inputListId) {
   const datalist = document.getElementById(dataListId);
   const inputlist = document.getElementById(inputListId);
@@ -181,62 +176,31 @@ function displayError(status, statusText) {
   document.getElementById('more-info').innerText = `Error ${status}: ${statusText}`;
 }
 
-// displayVisualization takes in a data array representing the 2D array
-// returned  by the census API. The first row in the census Data array
-// is the header, which describes the type of data.
-// Eg: Header for the query that finds for populations of all counties is
-// ["NAME","S0201_001E","state","county"]
-function displayVisualization(censusDataArray, description, title) {
-  const dataLength = censusDataArray[0].length;
-  // Check to see that it's not a county query
-  if (censusDataArray[0][dataLength - 1] !== 'county') {
-    google.charts.setOnLoadCallback(drawRegionsMap(censusDataArray,
-      description, title));
-  } else {
-    // We currently do not have counties implemented
-    displayError(400, 'We do not support this visualization yet.');
+function getLocationId(location, isCountyQuery) {
+  if (isCountyQuery) {
+    return location[2] + location[3];
   }
-}
-
-// Takes in a 2D array from the census API and displays the visualization
-function drawRegionsMap(censusDataArray, description, title) {
-  const shortDataArray = createDataArray(censusDataArray, description);
-  const data = google.visualization.arrayToDataTable(shortDataArray);
-  const options = {
-    'region': 'US',
-    'height': 550,
-    'resolution': 'provinces',
-    'colorAxis': {colors: ['white', 'blue']},
-  };
-  const mapElement = document.getElementById('map');
-  const chart = new google.visualization.GeoChart(mapElement);
-  chart.draw(data, options);
-  const mapNote = 'Populations are in thousands';
-  document.getElementById('more-info').innerHTML = mapNote;
+  return stateInfo[location[2]].ISO;
 }
 
 // createDataArray takes in the data array returned by the census API
 // and reformats it into a data table for the visualization API.
-function createDataArray(censusDataArray, description) {
+function createDataArray(censusDataArray, isCountyQuery) {
   const vizDataArray = [];
+  censusDataArray = censusDataArray.splice(1); // get rid of header row
   // Check to see if an extra calculation for percentages is needed
   if (checkPercentage(censusDataArray[0])) {
-    censusDataArray.forEach((state) => {
-      vizDataArray.push([state[0], percentToTotal(state[1], state[2])]);
+    censusDataArray.forEach((location) => {
+      vizDataArray.push({
+        id: getLocationId(location, isCountyQuery),
+        value: percentToTotal(location[1], location[2])});
     });
   } else {
-    censusDataArray.forEach((state) => {
-      vizDataArray.push([state[0], state[1]/1000]);
+    censusDataArray.forEach((location) => {
+      vizDataArray.push({
+        id: getLocationId(location, isCountyQuery),
+        value: location[1]});
     });
-  }
-  // Changes the header of the vizDataArray to match Visualization API
-  vizDataArray[0][0] = 'State';
-  // Add a more accurate descriptor for each state
-  vizDataArray[0][1] = 'Population of people ' +
-    description.age + ' who ' + description.action;
-  // make it moved to
-  if (description.action === 'moved') {
-    vizDataArray[0][1] += ' to';
   }
   return vizDataArray;
 }
@@ -265,9 +229,3 @@ function checkPercentage(headerColumn) {
   }
   return false;
 }
-
-function resizeVisualization() {
-  passQuery();
-}
-
-window.addEventListener('resize', resizeVisualization);
