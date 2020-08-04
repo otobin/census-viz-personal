edbeef1a0f6a9b25eab55aee5b478c8d642553d0
otobin <oliviatobin31@gmail.com>
2020-08-03 17:51:05 -0700

    Added geodata to state info file

--- src/main/webapp/state-info.js
+++ src/main/webapp/state-info.js
@@ -2,18 +2,22 @@ const stateInfo = {
     '01': {
         'name': 'Alabama',
         'ISO': 'US-AL',
+        geoData: am4geodata_region_usa_alLow,
     },
     '02': {
         'name': 'Alaska',
         'ISO': 'US-AK',
+        geoData: am4geodata_region_usa_akLow,
     },
     '04': {
         'name': 'Arizona',
         'ISO': 'US-AZ',
+        geoData: am4geodata_region_usa_azLow,
     },
     '05': {
         'name': 'Arkansas',
         'ISO': 'US-AR',
+        geoData: am4geodata_region_usa_arLow,
     },
     '06': {
         'name': 'California',
@@ -21,106 +25,132 @@ const stateInfo = {
         lat: 38,
         lng: -120,
         zoomLevel: 6,
+        geoData: am4geodata_region_usa_caLow,
     },
     '08': {
         'name': 'Colorado',
         'ISO': 'US-CO',
+        geoData: am4geodata_region_usa_coLow,
     },
     '09': {
         'name': 'Connecticut',
         'ISO': 'US-CT',
+        geoData: am4geodata_region_usa_ctLow,
     },
     '10': {
         'name': 'Delaware',
         'ISO': 'US-DE',
+        geoData: am4geodata_region_usa_deLow,
     },
     '11': {
         'name': 'District of Columbia',
         'ISO': 'US-DC',
+        geoData: am4geodata_region_usa_dcLow,
     },
     '12': {
         'name': 'Florida',
         'ISO': 'US-FL',
+        geoData: am4geodata_region_usa_flLow,
     },
     '13': {
         'name': 'Georgia',
         'ISO': 'US-GA',
+        geoData: am4geodata_region_usa_gaLow,
     },
     '15': {
         'name': 'Hawaii',
         'ISO': 'US-HI',
+        geoData: am4geodata_region_usa_hiLow,
     },
     '16': {
         'name': 'Idaho',
         'ISO': 'US-ID',
+        geoData: am4geodata_region_usa_idLow,
     },
     '17': {
         'name': 'Illinois',
         'ISO': 'US-IL',
+        geoData: am4geodata_region_usa_ilLow,
     },
     '18': {
         'name': 'Indiana',
         'ISO': 'US-IN',
+        geoData: am4geodata_region_usa_inLow,
     },
     '19': {
         'name': 'Iowa',
         'ISO': 'US-IA',
+        geoData: am4geodata_region_usa_iaLow,
     },
     '20': {
         'name': 'Kansas',
         'ISO': 'US-KS',
+        geoData: am4geodata_region_usa_ksLow,
     },
     '21': {
         'name': 'Kentucky',
         'ISO': 'US-KY',
+        geoData: am4geodata_region_usa_kyLow,
     },
     '22': {
         'name': 'Louisiana',
         'ISO': 'US-LA',
+        geoData: am4geodata_region_usa_laLow,
     },
     '23': {
         'name': 'Maine',
         'ISO': 'US-ME',
+        geoData: am4geodata_region_usa_meLow,
     },
     '24': {
         'name': 'Maryland',
         'ISO': 'US-MD',
+        geoData: am4geodata_region_usa_mdLow,
     },
     '25': {
         'name': 'Massachusetts',
         'ISO': 'US-MA',
+        geoData: am4geodata_region_usa_maLow,
     },
     '26': {
         'name': 'Michigan',
         'ISO': 'US-MI',
+        geoData: am4geodata_region_usa_miLow,
     },
     '27': {
         'name': 'Minnesota',
         'ISO': 'US-MN',
+        geoData: am4geodata_region_usa_mnLow,
     },
     '28': {
         'name': 'Mississippi',
         'ISO': 'US-MS',
+        geoData: am4geodata_region_usa_msLow,
     },
     '29': {
         'name': 'Missouri',
         'ISO': 'US-MO',
+        geoData: am4geodata_region_usa_moLow,
     },
     '30': {
         'name': 'Montana',
         'ISO': 'US-MT',
+        geoData: am4geodata_region_usa_mtLow,
     },
     '31': {
         'name': 'Nebraska',
         'ISO': 'US-NE',
+        geoData: am4geodata_region_usa_neLow,
     },
     '32': {
         'name': 'Nevada',
         'ISO': 'US-NV',
+        geoData: am4geodata_region_usa_nvLow,
     },
     '33': {
         'name': 'New Hampshire',
         'ISO': 'US-NH',
+        geoData: am4geodata_region_usa_nhLow,
     },
     '34': {
         'name': 'New Jersey',
@@ -128,89 +158,111 @@ const stateInfo = {
         lat: 40.25,
         lng: -75,
         zoomLevel: 8,
+        geoData: am4geodata_region_usa_nyLow,
     },
     '35': {
         'name': 'New Mexico',
         'ISO': 'US-NM',
+        geoData: am4geodata_region_usa_nmLow,
     },
     '36': {
         'name': 'New York',
         'ISO': 'US-NY',
+        geoData: am4geodata_region_usa_nyLow,
     },
     '37': {
         'name': 'North Carolina',
         'ISO': 'US-NC',
+        geoData: am4geodata_region_usa_ncLow,
     },
     '38': {
         'name': 'North Dakota',
         'ISO': 'US-ND',
+        geoData: am4geodata_region_usa_ndLow,
     },
     '39': {
         'name': 'Ohio',
         'ISO': 'US-OH',
+        geoData: am4geodata_region_usa_ohLow,
     },
     '40': {
         'name': 'Oklahoma',
         'ISO': 'US-OK',
+        geoData: am4geodata_region_usa_okLow,
     },
     '41': {
         'name': 'Oregon',
         'ISO': 'US-OR',
+        geoData: am4geodata_region_usa_orLow,
     },
     '42': {
         'name': 'Pennsylvania',
         'ISO': 'US-PA',
+        geoData: am4geodata_region_usa_paLow,
     },
     '44': {
         'name': 'Rhode Island',
         'ISO': 'US-RI',
+        geoData: am4geodata_region_usa_riLow,
     },
     '45': {
         'name': 'South Carolina',
         'ISO': 'US-SC',
+        geoData: am4geodata_region_usa_scLow,
     },
     '46': {
         'name': 'South Dakota',
         'ISO': 'US-SD',
+        geoData: am4geodata_region_usa_sdLow,
     },
     '47': {
         'name': 'Tennessee',
         'ISO': 'US-TN',
+        geoData: am4geodata_region_usa_tnLow,
     },
     '48': {
         'name': 'Texas',
         'ISO': 'US-TX',
+        geoData: am4geodata_region_usa_txLow,
     },
     '49': {
         'name': 'Utah',
         'ISO': 'US-UT',
+        geoData: am4geodata_region_usa_utLow,
     },
     '50': {
         'name': 'Vermont',
         'ISO': 'US-VT',
+        geoData: am4geodata_region_usa_vtLow,
     },
     '51': {
         'name': 'Virginia',
         'ISO': 'US-VA',
+        geoData: am4geodata_region_usa_vaLow,
     },
     '53': {
         'name': 'Washington',
         'ISO': 'US-WA',
+        geoData: am4geodata_region_usa_waLow,
     },
     '54': {
         'name': 'West Virginia',
         'ISO': 'US-WV',
+        geoData: am4geodata_region_usa_wvLow,
     },
     '55': {
         'name': 'Wisconsin',
         'ISO': 'US-WI',
+        geoData: am4geodata_region_usa_wiLow,
     },
     '56': {
         'name': 'Wyoming',
         'ISO': 'US-WY',
+        geoData: am4geodata_region_usa_wyLow,
     },
     '72': {
         'name': 'Puerto Rico',
         'ISO': 'US-PR',
+        geoData: am4geodata_region_usa_prLow,
     },
 };
