a73d79c50f5bd07ad044288ff4523c1ca2e2b672
kyracq <kyraa@google.com>
2020-08-07 09:45:48 -0400

    Switch to enable/disable

--- src/main/webapp/data-visualization.js
+++ src/main/webapp/data-visualization.js
@@ -283,24 +283,33 @@ function toggleMap() {
   if (checkbox.checked) {
     document.getElementById('am-charts').style.display = 'none';
     document.getElementById('map').style.display = 'block';
+    document.getElementById('map-toggle-msg').innerText = 'Disable map overlay';
   } else {
     document.getElementById('map').style.display = 'none';
     document.getElementById('am-charts').style.display = 'block';
+    document.getElementById('map-toggle-msg').innerText = 'Enable map overlay';
   }
 }
 
 // Sets up the webpage for the appropriate query.
 function setStyle(isCountyQuery) {
   const mapOptions = document.getElementById('map-options');
+  const chartsDiv = document.getElementById('am-charts');
+  const mapsDiv = document.getElementById('map');
   if (isCountyQuery) {
     mapOptions.style.display = 'block';
+    if (!document.getElementById('map-toggle').checked) {
+      chartsDiv.style.display = 'block';
+      mapsDiv.style.display = 'none';
+    } else {
+      mapsDiv.style.display = 'block';
+      chartsDiv.style.display = 'none';
+    }
   } else {
     mapOptions.style.display = 'none';
+    chartsDiv.style.display = 'block';
+    mapsDiv.style.display = 'none';
   }
-  const chartsDiv = document.getElementById('am-charts');
-  chartsDiv.style.display = 'block';
-  const mapsDiv = document.getElementById('map');
-  mapsDiv.style.display = 'none';
 }
 
 // Draw data table using Visualization API
