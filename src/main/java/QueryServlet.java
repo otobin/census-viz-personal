123270c74156848399acc3badf966d65134aa993
shulijones <srothbergjones@gmail.com>
2020-08-10 10:47:58 -0400

    Delete unnecessary import statement

--- src/main/java/QueryServlet.java
+++ src/main/java/QueryServlet.java
@@ -4,7 +4,6 @@ import com.google.gson.JsonObject;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.NoSuchFieldException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.Map;
