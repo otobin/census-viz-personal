d992cd99407e3bab337c7ed716c707a06dab812f
otobin <oliviatobin31@gmail.com>
2020-08-13 14:22:51 -0700

    Added comments

--- src/main/webapp/query-control.js
+++ src/main/webapp/query-control.js
@@ -202,31 +202,31 @@ async function createStateDropdownList() {
   });
 }
 
-// On page load, show appropriate icon based on the localStorage
-function loadAppropriateIcon() {
+// loadAppropriateIcon takes in a boolean buttonPressed. When buttonPressed is true, 
+// the location settings are set to the opposite of the current location settings 
+// and the opposite icon is shown. When buttonPressed is false, the location settings 
+// are not changed and the opposite icon is shown.
+function loadAppropriateIcon(buttonPressed) {
   const locationSettings = localStorage.getItem('locationSettings');
-  if (typeof locationSettings === 'undefined' || locationSettings === 'off') {
-    localStorage.setItem('locationSettings', 'off');
+  const locationOffString = 'Your location settings are currently set to off.' +
+    'Click here to change your location settings.';
+  const locationOnString = 'Your location settings are currently on.' + 
+    'Click here to change your location settings.';
+  if (((typeof locationSettings === 'undefined' || locationSettings === 'off') &&
+    (buttonPressed === false)) || (locationSettings === 'on' && buttonPressed === true)) {
+    // The user is turning their location off or it was already off and needs to 
+    // be reloaded on page refresh
+      localStorage.setItem('locationSettings', 'off');
     document.getElementById('location-on-icon').style.display = 'none';
     document.getElementById('location-off-icon').style.display = 'inline';
-  } else {
+    document.getElementById('location-id-text').innerText = locationOffString;
+  } else if (((typeof locationSettings === 'undefined' || locationSettings === 'off') &&
+  (buttonPressed === true)) || (locationSettings === 'on' && buttonPressed === false)) {
+    // The user is turning their location on or it was already on and needs to 
+    // be reloaded on page refresh
     localStorage.setItem('locationSettings', 'on');
     document.getElementById('location-off-icon').style.display = 'none';
     document.getElementById('location-on-icon').style.display = 'inline';
-  }
-}
-
-// When the icon is clicked, change the location settings to the opposite
-// and show the opposite icon
-function changeLocationSettings(icon) {
-  const iconId = icon.id;
-  if (iconId === 'location-on-icon') {
-    localStorage.setItem('locationSettings', 'off');
-    document.getElementById('location-on-icon').style.display = 'none';
-    document.getElementById('location-off-icon').style.display = 'inline';
-  } else {
-    localStorage.setItem('locationSettings', 'on');
-    document.getElementById('location-off-icon').style.display = 'none';
-    document.getElementById('location-on-icon').style.display = 'inline';
+    document.getElementById('location-id-text').innerText = locationOnString;
   }
 }
