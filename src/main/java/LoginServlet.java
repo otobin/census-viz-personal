8ff075379cf3a21842a618ecfeef9297be9c343d
kyracq <kyraa@google.com>
2020-08-14 19:07:43 -0400

    Lint fixes

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,9 +1,3 @@
-import com.google.appengine.api.datastore.DatastoreService;
-import com.google.appengine.api.datastore.DatastoreServiceFactory;
-import com.google.appengine.api.datastore.Entity;
-import com.google.appengine.api.datastore.EntityNotFoundException;
-import com.google.appengine.api.datastore.Key;
-import com.google.appengine.api.datastore.KeyFactory;
 import com.google.appengine.api.users.User;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
