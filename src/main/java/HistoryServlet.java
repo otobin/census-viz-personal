85547640fd8f5a0b14fd584b09fc7d2767d6069e
otobin <oliviatobin31@gmail.com>
2020-08-17 10:06:15 -0700

    Committing before switching

--- src/main/java/HistoryServlet.java
+++ src/main/java/HistoryServlet.java
@@ -8,14 +8,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
+import java.util.ArrayList;
 
 @WebServlet("/history")
 public class HistoryServlet extends HttpServlet {
 
     @Override 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        List<CensusData> history = ofy().load().type(CensusData.class).list();
-        String json = new Gson().toJson(history);
+        List<CensusData> allQueries = ofy().load().type(CensusData.class).list();
+        List<List<String>> queryDescriptionString = new ArrayList<List<String>>();
+        for (int i = 0; i < allQueries.size(); i++) {
+            queryDescriptionString.add(allQueries.get(i).getQueryList());
+        }
+        String json = new Gson().toJson(queryDescriptionString);
         response.getWriter().write(json);
     }
 }
