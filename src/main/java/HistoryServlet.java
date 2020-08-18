2987bddebcf4343cb6eab78c91ee12fd4d05ef9c
otobin <oliviatobin31@gmail.com>
2020-08-18 12:08:29 -0700

    added HistoryServlet

--- src/main/java/HistoryServlet.java
+++ src/main/java/HistoryServlet.java
@@ -28,8 +28,21 @@ public class HistoryServlet extends HttpServlet {
     UserService userService = UserServiceFactory.getUserService();
     if (userService.isUserLoggedIn()) {
       User user = userService.getCurrentUser();
-      String userId = user.getUserId();
+      String userId = (String) user.getUserId();
+      String personType = request.getParameter("person-type");
+      String action = request.getParameter("action");
+      String location = request.getParameter("location");
+      String year = request.getParameter("year");
+
+      Entity historyEntity = new Entity("historyEntity");
+      historyEntity.setProperty("userId", userId);
+      historyEntity.setProperty("personType", personType);
+      historyEntity.setProperty("action", action);
+      historyEntity.setProperty("location", location);
+      historyEntity.setProperty("year", year);
+
       DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+      datastore.put(historyEntity);
       Query query = new Query("historyEntity");
       PreparedQuery results = datastore.prepare(query);
 
@@ -37,45 +50,16 @@ public class HistoryServlet extends HttpServlet {
       for (Entity entity : results.asIterable()) {
           String entityUserId = (String) entity.getProperty("userId");
           if (entityUserId == userId) {
-            String personType = (String) entity.getProperty("personType");
-            String action = (String) entity.getProperty("action");
-            String location = (String) entity.getProperty("location");
-            String year = (String) entity.getProperty("year");
+            String entityPersonType = (String) entity.getProperty("personType");
+            String entityAction = (String) entity.getProperty("action");
+            String entityLocation = (String) entity.getProperty("location");
+            String entityYear = (String) entity.getProperty("year");
             List<String> queryPropertyList = Arrays.asList(personType, action, location, year);
             queryList.add(queryPropertyList);
           }
       }
       String json = new Gson().toJson(queryList);
       response.getWriter().write(json);
-    }
-  }
-
-  @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    UserService userService = UserServiceFactory.getUserService();
-    if (userService.isUserLoggedIn()) {
-        User user = userService.getCurrentUser();
-        String userId = (String) user.getUserId();
-        String personType = request.getParameter("person-type");
-        String action = request.getParameter("action");
-        String location = request.getParameter("location");
-        String year = request.getParameter("year");
-
-        Entity historyEntity = new Entity("historyEntity");
-        historyEntity.setProperty("userId", userId);
-        historyEntity.setProperty("personType", personType);
-        historyEntity.setProperty("action", action);
-        historyEntity.setProperty("location", location);
-        historyEntity.setProperty("year", year);
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(historyEntity);
-
-        response.getWriter().println("Yooo");
-        response.sendRedirect("/index.html");
-    } else {
-      response.getWriter().println("Yooo2");
-      response.sendRedirect("/index.html");
     }
   }
 }
