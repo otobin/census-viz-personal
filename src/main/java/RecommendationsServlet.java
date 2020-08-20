93f4d38b3db2105c35b7d33b7f4f2407609b84cc
otobin <oliviatobin31@gmail.com>
2020-08-20 14:53:31 -0700

    added beginning recommendation algorithm

--- src/main/java/RecommendationsServlet.java
+++ src/main/java/RecommendationsServlet.java
@@ -19,46 +19,29 @@ import java.lang.String;
 @WebServlet("/recommendations")
 public class RecommendationsServlet extends HttpServlet {
 
-  // Go through the datastore and map each 
-  public Map<String, Map<String, int>> getQueryFrequency(userId) {
-    Map<String, Map<String, int>> = new HashMap<String, Map<String, int>>();
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Go through datastore count the number of occurances 
+    // for all the queries from the user. 
+    Map<String, int> personTypeOccurances = new HashMap<String, integer>();
+    Map<String, int> actionOccurances = new HashMap<String, integer>();
+    Map<String, int> locationOccurances = new HashMap<String, integer>();
+    Map<String, int> yearOccurances = new HashMap<String, integer>();
     
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     Query query = new Query("historyEntity");
     PreparedQuery results = datastore.prepare(query);
-    for (Entity : results.asIterable()) {
+    for (Entity entity: results.asIterable()) {
         String entityUserId = (String) entity.getProperty("userId");
         if (entityUserId != null && userId != null) {
           if (entityUserId.equals(userId)) {
-
-    }
-  }
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    Query query = new Query("historyEntity");
-    PreparedQuery results = datastore.prepare(query);
-
-    List<HistoryElement> queryList = new ArrayList<HistoryElement>();
-    for (Entity entity : results.asIterable()) {
-      String entityUserId = (String) entity.getProperty("userId");
-        if (entityUserId != null && userId != null) {
-          if (entityUserId.equals(userId)) {
-            String entityPersonType = (String) entity.getProperty("personType");
-            String entityAction = (String) entity.getProperty("action");
-            String entityLocation = (String) entity.getProperty("location");
-            String entityYear = (String) entity.getProperty("year");
-            HistoryElement dataHistoryElement = new HistoryElement(entityUserId, 
-              entityPersonType,entityAction, entityLocation, entityYear);
-            // Check to see if it is already in the results to eliminate duplicates
-            if (!queryList.contains(dataHistoryElement)) {
-              queryList.add(dataHistoryElement);
+            entityPersonType = (String) entity.getProperty("personType");
+            if (personTypeOccurances.containsKey(entityPersonType)) {
+              personTypeOccurances.put(entityPersonType, map.get(entityPersonType) + 1);
+            } else {
+              personTypeOccurances.put(entityPersonType, 1);
             }
           }
-      }
     }
-    String json = new Gson().toJson(queryList);
-    response.getWriter().write(json);
   }
 }
