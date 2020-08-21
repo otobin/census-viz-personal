77861088c37e4d274daeb496b1a40065c3985726
otobin <oliviatobin31@gmail.com>
2020-08-20 22:10:15 -0700

    indented java

--- src/main/java/HistoryElement.java
+++ src/main/java/HistoryElement.java
@@ -43,8 +43,8 @@ public class HistoryElement {
     String otherLocation = otherElement.getLocation();
     String otherYear = otherElement.getYear();
     return ((this.personType.equals(otherPersonType))
-          && (this.action.equals(otherAction)) 
-          && (this.location.equals(otherLocation)) 
-          && (this.year.equals(otherYear)));
+            && (this.action.equals(otherAction)) 
+            && (this.location.equals(otherLocation)) 
+            && (this.year.equals(otherYear)));
   }
 }
