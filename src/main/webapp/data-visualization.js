981f153138247fe93b1655c9274818ace5aff77a
shulijones <srothbergjones@gmail.com>
2020-08-24 18:22:02 -0400

    Linting fixes

--- src/main/webapp/data-visualization.js
+++ src/main/webapp/data-visualization.js
@@ -86,27 +86,27 @@ function displayAmChartsMap(
   chart.exporting.filePrefix = 'CensusViz';
   chart.exporting.menu.items = [
     {
-      "label": "Download",
-      "menu": [
+      'label': 'Download',
+      'menu': [
         {
-          "label": "Image",
-          "menu": [
-            { "type": "png", "label": "PNG" },
-            { "type": "jpg", "label": "JPG" },
-            { "type": "svg", "label": "SVG" },
-            { "type": "pdf", "label": "PDF" }
-          ]
+          'label': 'Image',
+          'menu': [
+            {'type': 'png', 'label': 'PNG' },
+            {'type': 'jpg', 'label': 'JPG' },
+            {'type': 'svg', 'label': 'SVG' },
+            {'type': 'pdf', 'label': 'PDF' },
+          ],
         }, {
-          "label": "Data",
-          "menu": [
-            { "type": "json", "label": "JSON" },
-            { "type": "csv", "label": "CSV" },
-            { "type": "xlsx", "label": "XLSX" },
-            { "type": "html", "label": "HTML" },
-            { "type": "pdfdata", "label": "PDF" }
-          ]
+          'label': 'Data',
+          'menu': [
+            {'type': 'json', 'label': 'JSON' },
+            {'type': 'csv', 'label': 'CSV' },
+            {'type': 'xlsx', 'label': 'XLSX' },
+            {'type': 'html', 'label': 'HTML' },
+            {'type': 'pdfdata', 'label': 'PDF' },
+          ],
         }, {
-          "label": "Print", "type": "print"
+          'label': 'Print', 'type': 'print'
         }
       ]
     }
