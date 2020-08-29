ca46d368e29c789b28e7898fa9112e12d8c85bde
otobin <oliviatobin31@gmail.com>
2020-08-28 17:01:16 -0700

    linter history

--- src/main/java/History.java
+++ src/main/java/History.java
@@ -1,4 +1,5 @@
 package com.google.sps.data;
+
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 public class History {
   String userId;
   ArrayList<VisualizationData> historyList;
-  
+
   public History(String userId) {
     this.userId = userId;
     this.historyList = getHistory(userId);
@@ -37,13 +38,15 @@ public class History {
       String entityLocation = (String) entity.getProperty("location");
       String entityYear = (String) entity.getProperty("year");
       VisualizationData dataHistoryElement =
-        new VisualizationData(
-            entityUserId, entityPersonType, entityAction, entityLocation, entityYear);
+          new VisualizationData(
+              entityUserId, entityPersonType, entityAction, entityLocation, entityYear);
       // Check to see if it is already in the results to eliminate duplicates
       if (!queryList.contains(dataHistoryElement)) {
         queryList.add(dataHistoryElement);
       }
-    }
-    return queryList;
+      return queryList;
+    } 
   }
 }
+
+
